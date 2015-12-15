9ede929f8fd767f5dbff32d23e9af786b456b644
kalen <kalen5241@126.com>
2015-12-15 18:25:59 +0800

    通知练习

--- KalenStudyProject/KalenStudyProject/Utility/AppDelegate.m
+++ KalenStudyProject/KalenStudyProject/Utility/AppDelegate.m
@@ -65,7 +65,66 @@
     
     [self.window makeKeyAndVisible];
     
+    
+    if ([[UIApplication sharedApplication]currentUserNotificationSettings].types!=UIUserNotificationTypeNone) {
+        [self addLocalNotification];
+        
+        
+        UILocalNotification *notification=[launchOptions valueForKey:UIApplicationLaunchOptionsLocalNotificationKey];
+        NSDictionary *userInfo= notification.userInfo;
+        
+        [userInfo writeToFile:@"/Users/kalen/Desktop/didFinishLaunchingWithOptions.txt" atomically:YES];
+        NSLog(@"didFinishLaunchingWithOptions:The userInfo is %@.",userInfo);
+        
+        
+    }else{
+        [[UIApplication sharedApplication]registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound  categories:nil]];
+    }
+    
+    
+    
     return YES;
+}
+
+#pragma mark 接收本地通知时触发
+-(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
+    NSDictionary *userInfo=notification.userInfo;
+    [userInfo writeToFile:@"/Users/kalen/Desktop/didReceiveLocalNotification.txt" atomically:YES];
+    NSLog(@"didReceiveLocalNotification:The userInfo is %@",userInfo);
+}
+
+
+#pragma mark - 私有方法
+#pragma mark 添加本地通知
+-(void)addLocalNotification{
+    
+    //定义本地通知对象
+    UILocalNotification *notification=[[UILocalNotification alloc]init];
+    //设置调用时间
+    notification.fireDate=[NSDate dateWithTimeIntervalSinceNow:10.0];//通知触发的时间，10s以后
+    notification.repeatInterval=2;//通知重复次数
+    //notification.repeatCalendar=[NSCalendar currentCalendar];//当前日历，使用前最好设置时区等信息以便能够自动同步时间
+    
+    //设置通知属性
+    notification.alertBody=@"最近添加了诸多有趣的特性，是否立即体验？"; //通知主体
+    notification.applicationIconBadgeNumber=1;//应用程序图标右上角显示的消息数
+    notification.alertAction=@"打开应用"; //待机界面的滑动动作提示
+    notification.alertLaunchImage=@"Default";//通过点击通知打开应用时的启动图片,这里使用程序启动图片
+    //notification.soundName=UILocalNotificationDefaultSoundName;//收到通知时播放的声音，默认消息声音
+    notification.soundName=@"msg.caf";//通知声音（需要真机才能听到声音）
+    
+    //设置用户信息
+    notification.userInfo=@{@"id":@1,@"user":@"Kenshin Cui"};//绑定到通知上的其他附加信息
+    
+    //调用通知
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+}
+
+#pragma mark 调用过用户注册通知方法之后执行（也就是调用完registerUserNotificationSettings:方法之后执行）
+-(void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings{
+    if (notificationSettings.types!=UIUserNotificationTypeNone) {
+        [self addLocalNotification];
+    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -78,8 +137,10 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
+#pragma mark 进入前台后设置消息信息
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [[UIApplication sharedApplication]setApplicationIconBadgeNumber:0];//进入前台取消应用消息图标
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
@@ -89,6 +150,13 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+
+#pragma mark 移除本地通知，在不需要此通知时记得移除
+-(void)removeNotification{
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+}
+
 
 - (void)onlineConfigCallBack:(NSNotification *)note {
     
